1026588010c6043f6c35eb8fc66795ce5673f8cf
Sergei <tr4shslash@yandex.ru>
2024-06-25 21:57:45 +0300

    security api tested

--- stockAPI.py
+++ stockAPI.py
@@ -1,5 +1,26 @@
-from httpx im
+import httpx
+from httpx import Response, Request, AsyncClient
+import asyncio
+import json
 
 
 class API:
-    def __init__(self):
+    def __init__(self, client):
+        self._client = client
+
+    async def get_security_by_name(self, security_name: str):
+        params = {"q": security_name, "iss.meta": "off"}
+        response = await self._client.get(url="https://iss.moex.com/iss/securities.json", params=params)
+        r = json.loads(await response.aread())
+        result_list = list()
+        if "securities" in r:
+            columns = r["columns"]
+
+
+async def main():
+    async with httpx.AsyncClient() as ascl:
+        app = API(ascl)
+        await app.get_json_info("Apple")
+
+
+asyncio.run(main())
